7a256ab459d7116b94ce32213035200e241a30a7
Marc Reis <Marc.Reis.79@googlemail.com>
2016-09-11 22:17:59 +0200

    Add files via upload

---
+++ JavaUebung5/BruchTest.java
@@ -0,0 +1,81 @@
+package JavaUebung5;
+
+import javax.swing.*;
+
+/**
+ * Created by reisma on 05.09.16.
+ * Klasse zum Testen der "eigentlichen Klasse" Bruch
+ */
+public class BruchTest {
+
+
+    //ausführbare Methode (sonst kann man das Programm nicht starten)
+    public static void main (String[]args){
+
+        Bruch b = new Bruch();      // eine neue Instanz / Objekt aus der Klasse anlegen (Ein Bauplan, viele Objekte davon)
+
+        b.setZaheler(16);           // Werte für dieses eine Objekt setzen
+        b.setNenner(4);
+
+        b.gekuerztausgeben();       // Funktionen dieses Objekts nutzen
+
+        /** ------------------------------------------------------------------------------------------------
+         * Beispiel was Passiert wenn ich ein Objekt (Datentyp) anlege und einem andere zuweise vs. primitive Datentypen
+         * **/
+
+        Bruch c = new Bruch(3);     // Noch ein Objekt aus der Klasse anlegen (nun sind 2 im Speicher)
+
+        Bruch d = c;                // Objekte zeigen beim gleichsetzen auf den Selben speicherbereich
+
+        c.setNenner(9);             // Dieser eine wird nun so geändert das dort nun 1/9 steht, also steht es auch für
+                                    // d genauso da!
+
+        c.ausgeben();               // nun dieses c.ausgeben, welches anders als b "aussieht"
+        d.ausgeben();               // d, obwohl nicht direkt geändert ist wieder zu c identisch
+
+
+        int w = 5;                  // Bei Primitiven wird der Wert nur Kopiert
+        int y = w;                  //
+
+        System.out.println(w +"  "+ y); // 5 und 5 - beide haben den Wert 5
+
+        w = 10;                         // nur w ändert sich, y aber nicht es bleibt auf 5,
+                                        // oben beim Objekt wären nun beide auf 5!
+
+        System.out.println(w +"  "+ y);  // 10 und 5
+
+
+        /** ------------------------------------------------------------------------------------------------
+         * Ein möglich keit vom Benutzer werte für die Eingabe zu erhalten - hier mit eine Dialog Fenster
+         *
+         * **/
+
+        boolean doit = true;  // eine Variable um die while scheilfe laufen zu lassen
+
+        while (doit) { // Scheige die zu erst einmal immer weiter läuft - der Benutzer muss Sie abbrechen
+            int z = Integer.parseInt( JOptionPane.showInputDialog(null, "Zähler?") );   // Dialog für Nenner - Abfrage zeigen
+            int n = Integer.parseInt( JOptionPane.showInputDialog(null, "Nenner?"));
+
+            Bruch x = new Bruch(z,n);   // Eine neue Klasse erstellen und für den Konstruktor die Werte z,n übergeben
+
+            x.ausgeben(); // den Bruch ausgeben
+
+            x.gekuerztausgeben();   // den Bruch nun gekuertztausgeben lassen
+
+            // Abfragen zeigen um unsere boolean Variable zu setzen und ggf. die Schleife abzubrechen
+            if (JOptionPane.YES_OPTION != JOptionPane.showOptionDialog(null, "Noch eine?", "", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null))
+            {
+                doit = false;
+            }
+
+        }
+
+        /** ------------------------------------------------------------------------------------------------ **/
+        // Über das Statische Feld der Klasse (Das sich alle Objekte Teilen) kann man sehen wie viele Objekte angelegt wurden
+        System.out.println("Anzahl erstellter Brüche= "+Bruch.anzahlBrueche);
+
+
+
+    }
+
+}
