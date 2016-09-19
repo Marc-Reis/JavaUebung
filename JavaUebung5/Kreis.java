ccc4c866daae6461b83c57698cc6783bd303f2f3
Marc Reis <Marc.Reis.79@googlemail.com>
2016-09-19 11:01:16 +0200

    Added Example of the "Circle" class and its test class
    The Kreis class is used to define a simple class for representing circle object's.
    The test class uses this Kreis class and creates some circles before it ask the user to enter radius and radius enlargement factor via a JInputDialog. It then creates a table of 30 rows using this values and shows the radius, its extent and its area for each.

---
+++ JavaUebung5/Kreis.java
@@ -0,0 +1,54 @@
+package JavaUebung5;
+
+import javax.swing.text.TabStop;
+
+/**
+ * Created by reisma on 16.09.16.
+ */
+public class Kreis {
+
+    // --------- Hier kommen die Attribute der Klasse (Datenelement)
+
+    double radius;
+
+    // --------- Hier kommen die Konstruktoren ---------
+
+    public Kreis(){
+        setRadius(0);
+        // radius = 0; ging auch, aber wenn man get/set hat, dann besser diese nutzen!
+    }
+
+    // --------- Hier kommen die Methoden (auch get/set) ---------
+
+    double getRadius(){
+        return radius;
+    }
+
+    void setRadius(double r){
+        this.radius = r;
+    }
+
+    double getUmfang(){
+        return 2 * Math.PI * getRadius();
+    }
+
+    double getFlaeche(){
+        return 2 * Math.PI * Math.pow(radius, 2);
+    }
+
+    void setUmfang(double u){
+        setRadius( u /(2 * Math.PI) );
+    }
+
+    void setFlache(double f){
+        setRadius( Math.sqrt(f / (2*Math.PI) ) );
+    }
+
+
+    @Override
+    public String toString() {
+        return "Kreis Radius: "+getRadius() +"\t"
+                +" Umfang: "+getUmfang()+"\t"
+                +" Fläche: "+getFlaeche();
+    }
+}
